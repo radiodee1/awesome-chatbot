8ba05ff002317c9f46059024b6206b00be0bc492
David Liebman <david.c.liebman@gmail.com>
2018-02-17 17:34:23 -0500

    initial stuff

---
+++ model/model.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from settings import hparams
+
+print(hparams)
---
+++ model/settings.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+hparams = {
+    'save_dir': "../saved/",
+    'data_dir': "../data/",
+    'vocab_name': "vocab",
+    'test_name': "test",
+    'test_size': 100,
+    'train_name': "train",
+    'src_ending': "from",
+    'tgt_ending': "to",
+    'base_filename': "chatbot",
+    'num_train_total': 500000,
+    'batch_size': 256,
+    'steps_to_stats': 100,
+    'sol':'<s>',
+    'eol':'</s>',
+    'unk':'<unk>',
+    'layers': 2,
+    'units': 600,
+    'learning_rate': 0.001
+    
+}
