c6d7459785c839eef8a361c02f4365f67960a36b
David Liebman <david.c.liebman@gmail.com>
2018-03-05 16:52:42 -0500

    start categorical

---
+++ categorical/settings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+hparams = {
+    'save_dir': "../saved/",
+    'data_dir': "../data/",
+    'vocab_name': "vocab",
+    'test_name': "test",
+    'test_size': 100,
+    'train_name': "train",
+    'src_ending': "from",
+    'tgt_ending': "to",
+    'base_filename': "chatbot",
+    'base_file_num': 1,
+    'num_train_total': 500000,
+    'num_vocab_total': 20000,
+    'batch_size': 16, #256
+    'steps_to_stats': 100,
+    'sol':'sol',
+    'eol':'eol',
+    'unk':'unk',
+    'layers': 2,
+    'units': 64, ##600
+    'learning_rate': 0.001,
+    'tokens_per_sentence': 25,
+    'raw_embedding_filename': 'embedding',
+    'batch_constant': 10
+    
+}
