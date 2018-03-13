824a4221071034db203fd342a7b22f2f556f6f99
David Liebman <david.c.liebman@gmail.com>
2018-03-13 12:44:28 -0400

    remove do_prep_embedding.sh as other script does all vocab manipulation

--- categorical/do_prep_emmbedding.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FROM=../raw/glove.6B.100d.txt
-TO=../data/embed.txt
-
-
-python3 -c "from gensim.scripts.glove2word2vec import glove2word2vec;glove2word2vec(glove_input_file='$FROM', word2vec_output_file='$TO')"
-
-echo "file copied"
