59ef19dff093ec31cd1a2b351da8c20bb41ef196
David Liebman <david.c.liebman@gmail.com>
2018-03-19 16:59:57 -0400

    stop display after eol

--- model/model.py
+++ model/model.py
@@ -536,7 +536,7 @@ class ChatModel:
             for _ in range(repeats):
                 print('---basic predict---')
                 out = self.model.predict([source_input]) #, source_input])
-                print(out.shape)
+                #print(out.shape)
                 t_out = []
                 for i in range(tokens_per_sentence):
                     word = self.find_closest_word(out[0,i,:])
