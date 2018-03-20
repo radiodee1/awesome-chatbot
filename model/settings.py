cf721eb76b712be1319c44d8c2f15c5fb14e3673
David Liebman <david.c.liebman@gmail.com>
2018-03-20 16:53:41 -0400

    tokens_per_sentence = 24

--- model/settings.py
+++ model/settings.py
@@ -27,7 +27,7 @@ hparams = {
     'unk':'zzunkzz',
     'units': 256, #128, #256 , #64,
     'learning_rate': 0.001, #0.001
-    'tokens_per_sentence': 28, #32,
+    'tokens_per_sentence': 24, #32,
     'batch_constant': 512 #
     
 }
