f14cfd5dd64e339ab605a9621b18990adc6c40e9
David Liebman <david.c.liebman@gmail.com>
2018-03-20 16:51:15 -0400

    settings for units=256

--- do_make_vocab_copy.sh
+++ do_make_vocab_copy.sh
@@ -3,3 +3,4 @@
 
 cp data/vocab.from saved/saved_vocab.from
 cp data/vocab.to saved/saved_vocab.to
+cp data/embed.txt saved/saved_embed.txt
--- model/settings.py
+++ model/settings.py
@@ -10,24 +10,24 @@ hparams = {
     'train_name': "train",
     'src_ending': "from",
     'tgt_ending': "to",
-    'base_filename': "chatbot-att-no-shift",
+    'base_filename': "chatbot-big-vocab",
     'base_file_num': 1,
-    'num_vocab_total': 2000,
+    'num_vocab_total': 10000,
     'batch_size': 256,#64, #256
     'steps_to_stats': 100,
     'epochs': 100,
-    'embed_size':100, #values only: 50, 100, 200, 300
+    'embed_size':200, #values only: 50, 100, 200, 300
     'embed_train':False,
     'autoencode':True,
     'infer_repeat': 1,
     'embed_mode':'normal', #values only: mod, normal, zero
     'dense_activation':'tanh',#'tanh', #values only: tanh, relu, softmax, none
-    'sol':'sol',
-    'eol':'eol',
-    'unk':'unk',
-    'units': 128, #256 , #64,
+    'sol':'zzsolzz',
+    'eol':'zzeolzz',
+    'unk':'zzunkzz',
+    'units': 256, #128, #256 , #64,
     'learning_rate': 0.001, #0.001
-    'tokens_per_sentence': 32,
+    'tokens_per_sentence': 28, #32,
     'batch_constant': 512 #
     
 }
