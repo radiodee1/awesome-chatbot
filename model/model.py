2c2c73ba10472d6de6fce4e8dd3f4d05bb71e4b4
David Liebman <david.c.liebman@gmail.com>
2018-03-23 15:09:51 -0400

    change in comments only

--- model/model.py
+++ model/model.py
@@ -458,7 +458,8 @@ class ChatModel:
 
         adam = optimizers.Adam(lr=learning_rate)
 
-        # try 'categorical_crossentropy', 'mse', 'binary_crossentropy'
+        # loss try 'categorical_crossentropy', 'mse', 'binary_crossentropy'
+        # optimizer try 'rmsprop'
         model.compile(optimizer=adam, loss='mse',metrics=['acc'])
 
         return model , None, None #, None, model_inference
