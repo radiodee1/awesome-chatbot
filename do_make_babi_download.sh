62dacbd40040301b72ad3d0a9735d450e549d285
David Liebman <david.c.liebman@gmail.com>
2018-03-25 07:47:42 -0400

    comments

--- do_make_babi_download.sh
+++ do_make_babi_download.sh
@@ -4,3 +4,5 @@ cd raw
 wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz
 tar xvzf tasks_1-20_v1-2.tar.gz
 cp -r tasks_1-20_v1-2/en-valid-10k ./tasks
+
+## used for babi-input
