69927b75770329ed04d9dd5f5fad7a656783483d
David Liebman <david.c.liebman@gmail.com>
2018-04-04 13:06:37 -0400

    readme

--- do_make_vocab_copy.sh
+++ do_make_vocab_copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cp data/vocab.big.from saved/saved_vocab.big.from
+cp data/vocab.big.txt saved/saved_vocab.big.txt
 #cp data/vocab.to saved/saved_vocab.to
 cp data/embed.txt saved/saved_embed.txt
 cp model/settings.py saved/saved_settings.py.txt
