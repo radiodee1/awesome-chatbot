4be2c38e1bf78240a03100927173e1274c6b5226
David Liebman <david.c.liebman@gmail.com>
2018-04-05 12:09:43 -0400

    copy vocab.sh

--- do_make_vocab_copy.sh
+++ do_make_vocab_copy.sh
@@ -3,5 +3,5 @@
 
 cp data/vocab.big.txt saved/saved_vocab.big.txt
 #cp data/vocab.to saved/saved_vocab.to
-cp data/embed.txt saved/saved_embed.txt
 cp model/settings.py saved/saved_settings.py.txt
+cp data/embed.txt saved/saved_embed.txt
