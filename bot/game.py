8b057dffe8fbd69cb36f0ef125a23a482dff31b0
David Liebman <david.c.liebman@gmail.com>
2018-04-11 08:44:59 -0400

    game loop

--- bot/game.py
+++ bot/game.py
@@ -21,15 +21,52 @@ class Game:
         self.voice = v.VoiceOut()
         self.sr = sr.VoiceGoogleSR()
 
+        self.words_name = ['chatbot','mutter','robot']
+        self.words_start = ['start']
+        self.words_stop = ['stop','exit','quit','quiet','silence']
+        self.words_start += self.words_name
+        self.count_max = 5
 
     def loop(self):
+        count = 0
         while True:
-            #i = input('>')
             i = self.sr.voice_detection()
             i = tokenize_weak.format(i)
-            out = self.model.get_sentence(i)
-            print(out)
-            self.voice.speech_out(out)
+            if self.compare_sentence_to_list(i, self.words_start):
+                count = self.count_max
+                self.voice.speech_out('yes')
+                print('starting')
+            if self.compare_sentence_to_list(i, self.words_stop):
+                count = 0
+                print('stopping')
+            i = self.check_sentence(i)
+            if len(i) > 0:
+                if count > 0 :
+                    out = self.model.get_sentence(i)
+                    print(out)
+                    self.voice.speech_out(out)
+            count -= 1
+            if count <= 0 :
+                print('quiet')
+
+    def check_sentence(self, i):
+        i = i.split(' ')
+        out = []
+        for ii in i:
+            if ii in self.model.output_lang.word2index:
+                out.append(ii)
+        return ' '.join(out)
+
+    def compare_sentence_to_list(self, sent, l):
+        ## look for single word ##
+
+        i = sent.split(' ')
+        for ii in i:
+            if ii in l:
+                return True
+        return False
+
+
 
 if __name__ == '__main__':
     g = Game()
