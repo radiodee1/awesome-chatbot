ca4c44cb22dab6657bc9e42a4e9c84fbcf4f7bfa
David Liebman <david.c.liebman@gmail.com>
2018-04-12 15:45:04 -0400

    license

--- bot/game_sr.py
+++ bot/game_sr.py
@@ -11,7 +11,21 @@ from google.cloud.speech import enums
 from google.cloud.speech import types
 import pyaudio
 from six.moves import queue
-# [END import_libraries]
+
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Audio recording parameters
 RATE = 16000
@@ -81,7 +95,7 @@ class MicrophoneStream(object):
                     break
 
             yield b''.join(data)
-# [END audio_stream]
+
 
 
 class VoiceGoogleSR:
