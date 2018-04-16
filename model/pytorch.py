f456228cf13a860353b66acefc9964c8815427a3
David Liebman <david.c.liebman@gmail.com>
2018-04-16 14:30:21 -0400

    small edit

--- model/pytorch.py
+++ model/pytorch.py
@@ -147,26 +147,7 @@ class EncoderBiRNN(nn.Module):
         else:
             return result
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
-        self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-
-    def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
-        return output, hidden
-
-    def initHidden(self):
-        result = Variable(torch.zeros(1, 1, self.hidden_size))
-        if use_cuda:
-            return result.cuda()
-        else:
-            return result
 
 class DecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size):
