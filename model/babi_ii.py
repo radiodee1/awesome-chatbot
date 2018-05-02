208accc12291256815e6a84d673b4e044b1ad747
David Liebman <david.c.liebman@gmail.com>
2018-05-02 10:32:00 -0400

    attribution

--- model/babi_ii.py
+++ model/babi_ii.py
@@ -24,6 +24,33 @@ from settings import hparams
 
 
 '''
+Some code was originally written by Yerevann Research Lab. This theano code
+implements the DMN Network Model.
+
+https://github.com/YerevaNN/Dynamic-memory-networks-in-Theano
+
+The MIT License (MIT)
+
+Copyright (c) 2016 YerevaNN
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 Some code was originally written by Austin Jacobson. This refers specifically 
 to the Encoder and Decoder classes and came from:
 
