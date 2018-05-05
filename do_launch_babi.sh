b17134cf4b644ef6b109cb492839d887faf29c31
David Liebman <david.c.liebman@gmail.com>
2018-05-05 11:51:47 -0400

    clean up and edit readme.md

--- do_launch_babi.sh
+++ do_launch_babi.sh
@@ -1,3 +1,3 @@
 
 cd model
-python3.6 babi.py --load-babi --mode=train --use-filename
+python3.6 babi_ii.py $1 $2 $3 $4 $5 $6 $7 $8 $9
--- do_launch_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-cd model
-python3.6 model.py $1 $2 $3 $4 $5 $6
