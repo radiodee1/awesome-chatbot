5db79d94adb5e6485e2bb0d8c310a54507f30d95
David Liebman <david.c.liebman@gmail.com>
2018-06-07 19:40:44 -0400

    babi_iii.py is hybrid of custom and stock gru -- using adagrad -- tanh/sigmoid problem

--- model/babi_iii.py
+++ model/babi_iii.py
@@ -176,13 +176,13 @@ class EpisodicAttn(nn.Module):
         self.c_list_z = self.dropout_1(self.c_list_z)
 
         l_1 = torch.mm(self.W_c1, self.c_list_z) + self.b_c1
-        #l_1 = F.tanh(l_1)
-        l_1 = F.sigmoid(l_1)
+        l_1 = F.tanh(l_1)
+        #l_1 = F.sigmoid(l_1)
 
         l_2 = torch.mm(self.W_c2, l_1) + self.b_c2
         #l_2 = F.tanh(l_2)
-        l_2 = F.tanh(l_2)
-        #l_2 = F.sigmoid(l_2)
+        #l_2 = F.tanh(l_2)
+        l_2 = F.sigmoid(l_2)
         #l_3 = torch.mm(self.W_c3, l_2)
         l_3 = self.W_3(l_2)
 
