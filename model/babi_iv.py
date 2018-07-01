ac539e8f6ab84fcda3e02241f92acd931333a187
David Liebman <david.c.liebman@gmail.com>
2018-07-01 15:51:44 -0400

    print to screen function

--- model/babi_iv.py
+++ model/babi_iv.py
@@ -422,7 +422,7 @@ class WrapMemRNN(nn.Module):
 
             self.last_mem = mm_list
 
-            print(self.last_mem.size(),'lm')
+            #print(self.last_mem.size(),'lm')
 
         return None
 
@@ -517,19 +517,20 @@ class WrapMemRNN(nn.Module):
         #outputs
 
         ansx = self.model_5_ans(self.last_mem, None)
-        '''
-        print(ansx.size(), 'ansx')
-        vocab, sen = ansx.size()
-        aa = torch.argmax(ansx, dim=0)
-        print(aa.size(),'aa')
-        for i in range(sen):
-            zz = aa[i]
-            z = ansx[:, i]
-            a = torch.argmax(z, dim=0)
-            print(a.item(), zz.item())
-        print('----')
+
+        if self.print_to_screen:
+            print(ansx.size(), 'ansx')
+            vocab, sen = ansx.size()
+            aa = torch.argmax(ansx, dim=0)
+            print(aa.size(),'aa')
+            for i in range(sen):
+                zz = aa[i]
+                z = ansx[:, i]
+                a = torch.argmax(z, dim=0)
+                print(a.item(), zz.item())
+            print('----')
         #ans = torch.argmax(ansx,dim=1)#[0]
-        '''
+
 
         return [None], ansx
 
