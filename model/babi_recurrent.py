ab577e98920ca14efea0f7451944ddfe93658d05
David Liebman <david.c.liebman@gmail.com>
2018-09-03 13:26:48 -0400

    recurrent

--- model/babi_recurrent.py
+++ model/babi_recurrent.py
@@ -566,7 +566,7 @@ class AnswerModule(nn.Module):
             for i in range(self.maxtokens):
 
                 #output = F.softmax(output, dim=2)
-                output = F.tanh(output)
+                #output = F.tanh(output)
 
                 output, decoder_hidden = self.decoder(output, decoder_hidden)
 
