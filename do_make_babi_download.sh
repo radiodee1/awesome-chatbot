b66168285c2f0381570e7ebea2bd5062fb9fe896
David Liebman <david.c.liebman@gmail.com>
2018-09-28 18:04:47 -0400

    download etc

--- do_make_babi_download.sh
+++ do_make_babi_download.sh
@@ -6,5 +6,5 @@ wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz
 
 tar xvzf tasks_1-20_v1-2.tar.gz
 
-rm tasks_1-20_v1-2.tar.gz
+rm tasks_1-20_v1-2.tar.gz*
 ## not used ##
--- do_make_vocab_copy.sh
+++ do_make_vocab_copy.sh
@@ -6,3 +6,9 @@ cp data/vocab.babi.txt saved/saved_vocab.babi.txt
 #cp data/vocab.to saved/saved_vocab.to
 cp model/settings.py saved/saved_settings.py.txt
 cp data/embed.txt saved/saved_embed.txt
+
+cd saved
+zip vocab.zip saved*.txt
+rm saved*.txt
+
+mv vocab.zip ../
