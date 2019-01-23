07b9904f8fe9382a8e1f3dc2d59a66dfd9970dc2
David Liebman <david.c.liebman@gmail.com>
2019-01-23 15:26:52 -0500

    batch first

--- model/seq_2_seq.py
+++ model/seq_2_seq.py
@@ -307,7 +307,7 @@ class Decoder(nn.Module):
             gru_in_dim = hidden_dim
             linear_in_dim = hidden_dim
 
-        self.gru = nn.GRU(gru_in_dim, hidden_dim, self.n_layers, dropout=dropout, batch_first=True)
+        self.gru = nn.GRU(gru_in_dim, hidden_dim, self.n_layers, dropout=dropout, batch_first=True, bidirectional=False)
         self.out_target = nn.Linear(hidden_dim, target_vocab_size)
 
         self.out_concat = nn.Linear(linear_in_dim, hidden_dim)
@@ -341,7 +341,7 @@ class Decoder(nn.Module):
 
             decoder_hidden_x = prune_tensor(decoder_hidden_x, 3)
             #decoder_hidden_x = decoder_hidden_x.permute(1,0,2)
-            decoder_hidden_x = decoder_hidden_x[:,  self.n_layers:, :]
+            decoder_hidden_x = decoder_hidden_x[:,  self.n_layers :, :]
 
         decoder_hidden_x = torch.relu(decoder_hidden_x)
 
