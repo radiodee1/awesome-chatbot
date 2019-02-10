5ba48503335df02580f5b129f9236d58ee73d085
David Liebman <david.c.liebman@gmail.com>
2019-02-10 15:50:15 -0500

    dot method for attn

--- model/seq_2_seq_beam.py
+++ model/seq_2_seq_beam.py
@@ -353,7 +353,7 @@ class Encoder(nn.Module):
 
 class Attn(torch.nn.Module):
     def __init__(self,  hidden_size):
-        method = 'general'
+        method = 'dot' #'general'
         super(Attn, self).__init__()
         self.method = method
         if self.method not in ['dot', 'general', 'concat']:
