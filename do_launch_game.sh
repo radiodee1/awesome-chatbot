dd5aa2403eeb441017293c9f832447f1187cb779
David Liebman <david.c.liebman@gmail.com>
2019-02-24 14:13:11 -0500

    launcher script

--- do_launch_game.sh
+++ do_launch_game.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+## typical configurations
+TEST_1="--mode=long --basename=test_s2s_long_b_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
+TEST_2="--mode=long --basename=test_s2s_freeze_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk --load-embed-size=300 --freeze-embedding"
+TEST_3="--mode=long --basename=test_s2s_long_b_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
+
+
 cd bot
-python3.6 game.py --mode=long --basename=test_s2s_long_b_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk  2> /dev/null
+python3.6 game.py $TEST_1  2> /dev/null
