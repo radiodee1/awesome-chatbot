751521764e3f164d4d34e5ef191b80d233d1e3e4
David Liebman <david.c.liebman@gmail.com>
2019-02-24 16:07:42 -0500

    launcher script

--- do_launch_game.sh
+++ do_launch_game.sh
@@ -3,7 +3,7 @@
 ## typical configurations
 TEST_1="--mode=long --basename=test_s2s_long_b_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
 TEST_2="--mode=long --basename=test_s2s_freeze_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk --load-embed-size=300 --freeze-embedding"
-TEST_3="--mode=long --basename=test_s2s_long_b_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
+TEST_3="--mode=long --basename=test_s2s_size_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
 
 
 cd bot
