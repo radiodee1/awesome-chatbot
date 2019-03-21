d1a86820c8445efd9c7648b4512fcfd7b422b698
David Liebman <david.c.liebman@gmail.com>
2019-03-21 09:42:42 -0400

    t2t export

---
+++ model/tens_2_tens_export.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3.6
+
+import sys
+sys.path.append('..')
+from tensor2tensor.serving import export
+from model.settings import hparams
+import tensorflow as tf
+
+
+
+problem = 'chat_line_problem'
+outdir = hparams['save_dir'] + '/t2t_trained_model/'
+train_steps = str(7500) ## 7500
+
+args_train = [
+    '--t2t_usr_dir=' + './transformer/',
+    '--problem=' + problem,
+    '--data_dir=' + hparams['data_dir'] + '/t2t_data/',
+
+    '--model=' + 'transformer',
+    '--output_dir=' + outdir , #hparams['save_dir'] + '/t2t_export/',
+    '--hparams_set=' + 'transformer_chat',
+    #'--job_dir=' + outdir,
+    #'--train_steps=' + train_steps,
+
+]
+
+
+
+def main(argv):
+
+    export.main(argv)
+
+if __name__ == "__main__":
+
+    sys.argv.extend(args_train[:])
+    print(sys.argv)
+
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    tf.app.run()
+
+
