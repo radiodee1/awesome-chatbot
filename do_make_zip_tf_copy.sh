22882ce42b6979c70bdd1e5506fbb7239a2dcdc1
David Liebman <david.c.liebman@gmail.com>
2019-03-24 09:04:37 -0400

    start server with nmt main()

--- do_make_zip_tf_copy.sh
+++ do_make_zip_tf_copy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-zip -r tens_2_tens.zip saved/t2t_trained_model/export/* data/t2t_data/vocab*
+echo $#
+echo $@
+
+
+zip -r tens_2_tens.zip saved/t2t_trained_model/export/* data/t2t_data/vocab* $@
+
+echo "use this script with any savable files as the parameter."
