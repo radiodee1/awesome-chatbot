1a3ecddd4cd8eb82490c1466007b13d2d8060e63
David Liebman <david.c.liebman@gmail.com>
2019-03-28 19:03:13 -0400

    bert stuff

---
+++ model/bert_server_start.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.6
+
+
+import re
+import sys
+
+from bert_serving.server.cli import main
+
+args_bert = ["-model_dir=../data/bert_data/uncased_L-12_H-768_A-12/" , "-num_worker=4"]
+
+if __name__ == '__main__':
+
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.argv.extend(args_bert)
+    sys.exit(main())
