11f5df753e0643356501a48c09625f19ec9b757d
David Liebman <david.c.liebman@gmail.com>
2019-04-09 14:24:11 -0400

    classifier with chat class and functions

--- model/bert_classifier.py
+++ model/bert_classifier.py
@@ -429,33 +429,58 @@ class ChatProcessor(DataProcessor):
     def get_dev_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "dev")
 
     def get_test_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "test")
 
     def get_labels(self):
         """See base class."""
-        return ["0", "1"]
+        labels = [ i for i in " ;:-'\",.!?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+        print(labels)
+        return labels #["0", "1"]
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
         examples = []
+        num = 0
+        print(len(lines), 'lines')
+        split_start = 0
+        split_train = 1000 if len(lines) > 1000 else 0
+        split_test = 500 if len(lines) > 500 else 0
+        if set_type == "dev":
+            split_start = split_test
+            split_test = split_test * 2
+
         for (i, line) in enumerate(lines):
-            # Only the test set has a header
-            if set_type == "test" and i == 0:
-                continue
-            guid = "%s-%s" % (set_type, i)
-            if set_type == "test":
-                text_a = tokenization.convert_to_unicode(line[1])
-                label = "0"
-            else:
-                text_a = tokenization.convert_to_unicode(line[3])
-                label = tokenization.convert_to_unicode(line[1])
-            examples.append(
-                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+
+            guid = num
+            if set_type != "train" :
+                if num > split_start:
+                    text_a = tokenization.convert_to_unicode(line[0])
+                    label = " "
+                    examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+                if num > split_test :
+                    break
+            elif set_type == "train" :
+                if num > split_train:
+                    train = []
+                    text_b = tokenization.convert_to_unicode(line[1])
+
+                    for z in range(len(text_b)):
+                        text_a = tokenization.convert_to_unicode(line[0])
+
+                        txt = text_b[:z]
+
+                        label = text_b[z]
+                        #if set_type != 'train': label = ' '
+                        text_a = text_a + " " + txt
+                        train.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+                    examples.extend(train)
+
+            num += 1
         return examples
 
 def convert_single_example(ex_index, example, label_list, max_seq_length,
