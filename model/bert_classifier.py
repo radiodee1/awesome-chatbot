f04de2ab55f2acf0e194001e687ed152689d7131
David Liebman <david.c.liebman@gmail.com>
2019-04-20 16:28:52 -0400

    add mask to classifier

--- model/bert_classifier.py
+++ model/bert_classifier.py
@@ -727,7 +727,9 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
         for token in tokens_b:
             tokens.append(token)
             segment_ids.append(1)
+        tokens.append("[MASK]") ## mask
         tokens.append("[SEP]")
+        segment_ids.append(1) ## mask
         segment_ids.append(1)
 
     input_ids = tokenizer.convert_tokens_to_ids(tokens)
@@ -935,68 +937,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
         return (loss, per_example_loss, logits, probabilities)
 
-'''
-def model_fn_builder_v2(bert_config, init_checkpoint, layer_indexes, use_tpu,
-                     use_one_hot_embeddings):
-    ##Returns `model_fn` closure for TPUEstimator.
 
-    def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
-        ##The `model_fn` for TPUEstimator.
-
-        unique_ids = features["unique_ids"]
-        input_ids = features["input_ids"]
-        input_mask = features["input_mask"]
-        input_type_ids = features["input_type_ids"]
-
-        model = modeling.BertModel(
-            config=bert_config,
-            is_training=False,
-            input_ids=input_ids,
-            input_mask=input_mask,
-            token_type_ids=input_type_ids,
-            use_one_hot_embeddings=use_one_hot_embeddings)
-
-        if mode != tf.estimator.ModeKeys.PREDICT:
-            raise ValueError("Only PREDICT modes are supported: %s" % (mode))
-
-        tvars = tf.trainable_variables()
-        scaffold_fn = None
-        (assignment_map,
-         initialized_variable_names) = modeling.get_assignment_map_from_checkpoint(
-            tvars, init_checkpoint)
-        if use_tpu:
-
-            def tpu_scaffold():
-                tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-                return tf.train.Scaffold()
-
-            scaffold_fn = tpu_scaffold
-        else:
-            tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-
-        tf.logging.info("**** Trainable Variables ****")
-        for var in tvars:
-            init_string = ""
-            if var.name in initialized_variable_names:
-                init_string = ", *INIT_FROM_CKPT*"
-            tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
-                            init_string)
-
-        all_layers = model.get_all_encoder_layers()
-
-        predictions = {
-            "unique_id": unique_ids,
-        }
-
-        for (i, layer_index) in enumerate(layer_indexes):
-            predictions["layer_output_%d" % i] = all_layers[layer_index]
-
-        output_spec = tf.contrib.tpu.TPUEstimatorSpec(
-            mode=mode, predictions=predictions, scaffold_fn=scaffold_fn)
-        return output_spec
-
-    return model_fn
-'''
 
 def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
                      num_train_steps, num_warmup_steps, use_tpu,
