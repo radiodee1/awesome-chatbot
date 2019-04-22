a3a275f987987820f039cb8991e70fdfe7fe8670
David Liebman <david.c.liebman@gmail.com>
2019-04-22 13:52:45 -0400

    launch code

--- do_launch_game.sh
+++ do_launch_game.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 ## typical configurations
-TEST_1="--mode=long --basename=test_s2s_long_b_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
-TEST_2="--mode=long --basename=test_s2s_freeze_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk --load-embed-size=300 --freeze-embedding"
-TEST_3="--mode=long --basename=test_s2s_size_d300_v15000 --load-babi --lr=0.0001 --dropout=0.5 --recurrent-output --load-recurrent --hide-unk --units=300 --skip-unk"
 
 TEST_4="--mode=long --basename=test_s2s_movie_openvocab_d300_v15000_len15 --load-babi --lr=0.001 --dropout=0.5 --load-recurrent --units=300 --record-loss --multiplier=0.5 --length=15 --no-vocab "
 TEST_5="--mode=long --basename=test_s2s_general_d300_v15000_length15 --load-babi --lr=0.001 --dropout=0.5 --load-recurrent --units=300 --record-loss --multiplier=0.5 --length=15 --skip-unk --hide-unk"
