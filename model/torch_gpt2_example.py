f038e9f522bfa0186ae7afb2bfccde5982cabad2
David Liebman <david.c.liebman@gmail.com>
2019-04-24 18:22:47 -0400

    gpt2

--- model/torch_gpt2_example.py
+++ model/torch_gpt2_example.py
@@ -13,49 +13,65 @@ from pytorch_pretrained_bert import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
 import logging
 logging.basicConfig(level=logging.INFO)
 
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+class GPT2_small:
+    def __init__(self):
 
-# Load pre-trained model (weights)
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-model.eval()
+        self.tokenizer = None
+        self.model = None
+        self.wordcount = 15
+        self.words_end = ['.', '?', '!']
+
+        self.past = None
+
+    def setup_for_interactive(self):
+        # Load pre-trained model tokenizer (vocabulary)
+        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+        # Load pre-trained model (weights)
+        self.model = GPT2LMHeadModel.from_pretrained('gpt2')
+        self.model.eval()
+
+    def get_sentence(self, i):
+        num = 0
+        text_1 = i
+        text_2 = ""
+        self.past = None
+        decode_list = []
+        while num < self.wordcount:
+
+            indexed_tokens_2 = self.tokenizer.encode(text_1 + " . " + text_2)
+            tokens_tensor_2 = torch.tensor([indexed_tokens_2])
+
+            with torch.no_grad():
+                predictions_1, self.past = self.model(tokens_tensor_2, past=self.past)
+
+            predicted_index = torch.argmax(predictions_1[0, -1, :]).item()
+            decode_list.append(predicted_index)
+            predicted_token = self.tokenizer.decode([predicted_index])
+
+            print(text_1 + ' - ' + text_2.strip('\n'), '[', predicted_index, '-' + predicted_token + '-', ']')
+
+            text_2 += predicted_token
+
+            if predicted_token.strip() in self.words_end or predicted_token[0] in self.words_end:
+                # past = None
+                break
+            num += 1
+        print(text_2)
+        return text_2
 
 
-past = None
+    def loop(self):
 
-while True:
-    num = 0
-    text_1 = input(">>> ")
-    text_2 = ""
-    past = None
-    decode_list = []
-    while num < 10:
-        #past = None
+        self.past = None
 
-        indexed_tokens_2 = tokenizer.encode(text_1 + " . " + text_2)
-
-        # Convert inputs to PyTorch tensors
-
-        tokens_tensor_2 = torch.tensor([indexed_tokens_2])
-
-        with torch.no_grad():
-
-            predictions_1, past = model(tokens_tensor_2, past=past)
-
-        predicted_index = torch.argmax(predictions_1[0, -1, :]).item()
-        decode_list.append(predicted_index)
-        predicted_token = tokenizer.decode([predicted_index])
+        while True:
+            #num = 0
+            text_1 = input(">>> ")
+            self.get_sentence(text_1)
 
 
-        print(text_1 + ' - ' +  text_2.strip('\n'), '[', predicted_index,'-' +predicted_token + '-', ']')
-        #if len(predicted_token.strip()) > 0 or True:
-        #text_2 += " " + predicted_token
-
-        text_2 += predicted_token
-
-        if predicted_token.strip() in ['.','?','!'] or predicted_token[0]  in ['.','?','!']:
-            #past = None
-            break
-        num += 1
-    print(text_2)
-    #print(tokenizer.decode(decode_list))
+if __name__ == '__main__':
+    g = GPT2_small()
+    g.setup_for_interactive()
+    g.loop()
