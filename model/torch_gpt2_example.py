f8ab3de7311d0d186d33f540e5c6bb316cba70c0
David Liebman <david.c.liebman@gmail.com>
2019-04-26 07:40:34 -0400

    example.py

--- model/torch_gpt2_example.py
+++ model/torch_gpt2_example.py
@@ -6,12 +6,19 @@
 #  year={2019}
 #}
 
+import sys
+sys.path.append('..')
+from model.settings import hparams
 import torch
 from pytorch_pretrained_bert import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
 import random
 # OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
 import logging
 logging.basicConfig(level=logging.INFO)
+
+gpt2_dir = hparams['data_dir'] + '/' + 'gpt2' + '/'
+
+## vocab.json merges.txt
 
 class Lang:
     def __init__(self, name, limit=None):
@@ -51,10 +58,10 @@ class NMT:
 
     def setup_for_interactive(self):
         # Load pre-trained model tokenizer (vocabulary)
-        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        self.tokenizer = GPT2Tokenizer.from_pretrained(gpt2_dir)
 
         # Load pre-trained model (weights)
-        self.model = GPT2LMHeadModel.from_pretrained('gpt2')
+        self.model = GPT2LMHeadModel.from_pretrained(gpt2_dir)
         self.model.eval()
 
         print(self.tokenizer.__len__(),'max')
@@ -81,7 +88,7 @@ class NMT:
 
             #print(num, text_1 + ' - ' + text_2.strip('\n'), '[', predicted_index, '-' + predicted_token + '-', ']')
 
-            text_2 += predicted_token 
+            text_2 += predicted_token
 
             if predicted_token.strip() in self.words_end or predicted_token[0] in self.words_end:
                 break
@@ -94,40 +101,7 @@ class NMT:
         print(text_2)
         return text_2
 
-    '''
-    def get_sentence_v2(self, i):
-        num = 0
-        text_1 = i
-        text_2 = ""
-        self.past = None
-        # decode_list = []
-        #while num < self.wordcount:
 
-        indexed_tokens_2 = self.tokenizer.encode(text_1 + " ? " + text_2)
-        tokens_tensor_2 = torch.tensor([indexed_tokens_2])
-
-        with torch.no_grad():
-            predictions_1, self.past = self.model(tokens_tensor_2, past=self.past)
-
-        while num < self.wordcount:
-            predicted_index = torch.argmax(predictions_1[0, num, :]).item()
-            predicted_token = self.tokenizer.decode([predicted_index])
-
-            print(num, text_1 + ' - ' + text_2.strip('\n'), '[', predicted_index, '-' + predicted_token + '-', ']')
-
-            text_2 += predicted_token  # .strip()
-
-            if predicted_token.strip() in self.words_end or predicted_token[0] in self.words_end:
-                break
-            num += 1
-
-        if text_2.strip().lower().endswith(text_1.strip().lower()):
-            print('endswith')
-            text_2 = text_2[: - len(text_1)]
-        print(text_2)
-
-        return text_2
-    '''
 
     def loop(self):
 
@@ -138,7 +112,7 @@ class NMT:
             try:
                 text_1 = input(">>> ")
             except EOFError:
-                print('end of file.')
+                print('\nend of file.')
                 exit()
             print(text_1)
             self.get_sentence(text_1)
