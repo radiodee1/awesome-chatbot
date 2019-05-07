29e3674e2fd266d1f96653fbf0a6ab72cdf0f427
David Liebman <david.c.liebman@gmail.com>
2019-05-07 15:43:06 -0400

    use gpt2 in run file.

--- model/torch_gpt2_run.py
+++ model/torch_gpt2_run.py
@@ -1,133 +1,101 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-import argparse
-import logging
-from tqdm import trange
-
+'''
+    code by TaeHwan Jung(@graykode)
+    Original Paper and repository here : https://github.com/openai/gpt-2
+    GPT2 Pytorch Model : https://github.com/huggingface/pytorch-pretrained-BERT
+'''
+import os
+import sys
+sys.path.append('torch_gpt2')
 import torch
-import torch.nn.functional as F
+import random
+import argparse
 import numpy as np
+import json
+#import regex as re
+#from functools import lru_cache
+from GPT2.model import (GPT2LMHeadModel)
+from GPT2.utils import load_weight
+from GPT2.config import GPT2Config
+from GPT2.sample import sample_sequence
+#from GPT2.encoder import get_encoder
+from GPT2.encoder import Encoder
 
-from pytorch_pretrained_bert import GPT2LMHeadModel, GPT2Tokenizer
+def get_encoder():
+    with open('./torch_gpt2/GPT2/encoder.json', 'r') as f:
+        encoder = json.load(f)
+    with open('./torch_gpt2/GPT2/vocab.bpe', 'r', encoding="utf-8") as f:
+        bpe_data = f.read()
+    bpe_merges = [tuple(merge_str.split()) for merge_str in bpe_data.split('\n')[1:-1]]
+    return Encoder(
+        encoder=encoder,
+        bpe_merges=bpe_merges,
+    )
 
-logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level = logging.INFO)
-logger = logging.getLogger(__name__)
-
-def top_k_logits(logits, k):
-    """
-    Masks everything but the k top entries as -infinity (1e10).
-    Used to mask logits such that e^-infinity -> 0 won't contribute to the
-    sum of the denominator.
-    """
-    if k == 0:
-        return logits
-    else:
-        values = torch.topk(logits, k)[0]
-        batch_mins = values[:, -1].view(-1, 1).expand_as(logits)
-        return torch.where(logits < batch_mins, torch.ones_like(logits) * -1e10, logits)
-
-def sample_sequence(model, length, start_token=None, batch_size=None, context=None, temperature=1, top_k=0, device='cuda', sample=True):
-    if start_token is None:
-        assert context is not None, 'Specify exactly one of start_token and context!'
-        context = torch.tensor(context, device=device, dtype=torch.long).unsqueeze(0).repeat(batch_size, 1)
-    else:
-        assert context is None, 'Specify exactly one of start_token and context!'
-        context = torch.full((batch_size, 1), start_token, device=device, dtype=torch.long)
-    prev = context
-    output = context
-    past = None
-    with torch.no_grad():
-        for i in trange(length):
-            logits, past = model(prev, past=past)
-            logits = logits[:, -1, :] / temperature
-            logits = top_k_logits(logits, k=top_k)
-            log_probs = F.softmax(logits, dim=-1)
-            if sample:
-                prev = torch.multinomial(log_probs, num_samples=1)
-            else:
-                _, prev = torch.topk(log_probs, k=1, dim=-1)
-            output = torch.cat((output, prev), dim=1)
-    return output
-
-def run_model():
+def text_generator(state_dict):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name_or_path', type=str, default='gpt2', help='pretrained model name or path to local checkpoint')
-    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--text", type=str, required=True)
+    parser.add_argument("--quiet", type=bool, default=False)
     parser.add_argument("--nsamples", type=int, default=1)
+    parser.add_argument('--unconditional', action='store_true', help='If true, unconditional generation.')
     parser.add_argument("--batch_size", type=int, default=-1)
     parser.add_argument("--length", type=int, default=-1)
-    parser.add_argument("--temperature", type=int, default=1)
-    parser.add_argument("--top_k", type=int, default=0)
-    parser.add_argument('--unconditional', action='store_true', help='If true, unconditional generation.')
+    parser.add_argument("--temperature", type=float, default=0.7)
+    parser.add_argument("--top_k", type=int, default=40)
     args = parser.parse_args()
-    print(args)
+
+    if args.quiet is False:
+        print(args)
 
     if args.batch_size == -1:
         args.batch_size = 1
     assert args.nsamples % args.batch_size == 0
 
-    np.random.seed(args.seed)
-    torch.random.manual_seed(args.seed)
-    torch.cuda.manual_seed(args.seed)
+    seed = random.randint(0, 2147483647)
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    enc = GPT2Tokenizer.from_pretrained(args.model_name_or_path)
-    model = GPT2LMHeadModel.from_pretrained(args.model_name_or_path)
+    # Load Model
+    enc = get_encoder()
+    config = GPT2Config()
+    model = GPT2LMHeadModel(config)
+    model = load_weight(model, state_dict)
     model.to(device)
     model.eval()
 
     if args.length == -1:
-        args.length = model.config.n_ctx // 2
-    elif args.length > model.config.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % model.config.n_ctx)
+        args.length = config.n_ctx // 2
+    elif args.length > config.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % config.n_ctx)
 
-    while True:
-        context_tokens = []
-        if not args.unconditional:
-            raw_text = input("Model prompt >>> ")
-            while not raw_text:
-                print('Prompt should not be empty!')
-                raw_text = input("Model prompt >>> ")
-            context_tokens = enc.encode(raw_text)
-            generated = 0
-            for _ in range(args.nsamples // args.batch_size):
-                out = sample_sequence(
-                    model=model, length=args.length,
-                    context=context_tokens,
-                    start_token=None,
-                    batch_size=args.batch_size,
-                    temperature=args.temperature, top_k=args.top_k, device=device
-                )
-                out = out[:, len(context_tokens):].tolist()
-                for i in range(args.batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
+    print(args.text)
+    context_tokens = enc.encode(args.text)
 
-            print("=" * 80)
-        if args.unconditional:
-            generated = 0
-            for _ in range(args.nsamples // args.batch_size):
-                out = sample_sequence(
-                    model=model, length=args.length,
-                    context=None,
-                    start_token=enc.encoder['<|endoftext|>'],
-                    batch_size=args.batch_size,
-                    temperature=args.temperature, top_k=args.top_k, device=device
-                )
-                out = out[:,1:].tolist()
-                for i in range(args.batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
-            print("=" * 80) ## ?
-            if args.unconditional: ## ?
-                break ## ?
+    generated = 0
+    for _ in range(args.nsamples // args.batch_size):
+        out = sample_sequence(
+            model=model, length=args.length,
+            context=context_tokens  if not  args.unconditional else None,
+            start_token=enc.encoder['<|endoftext|>'] if args.unconditional else None,
+            batch_size=args.batch_size,
+            temperature=args.temperature, top_k=args.top_k, device=device
+        )
+        out = out[:, len(context_tokens):].tolist()
+        for i in range(args.batch_size):
+            generated += 1
+            text = enc.decode(out[i])
+            if args.quiet is False:
+                print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+            print(text)
 
 if __name__ == '__main__':
-    run_model()
-
+    p = './torch_gpt2/gpt2-pytorch_model.bin'
+    if os.path.exists(p):
+        state_dict = torch.load(p, map_location='cpu' if not torch.cuda.is_available() else None)
+        text_generator(state_dict)
+    else:
+        print('Please download gpt2-pytorch_model.bin')
+        sys.exit()
