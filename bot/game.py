2559b07422280f01d93abc13cdbd8665066ba11a
David Liebman <david.c.liebman@gmail.com>
2019-05-20 13:44:26 -0400

    set name to Kay

--- bot/game.py
+++ bot/game.py
@@ -39,7 +39,7 @@ class Game:
         self.voice = v.VoiceOut()
         self.sr = sr.VoiceGoogleSR()
 
-        self.words_name = ['chatbot','mutter','robot']
+        self.words_name = ['chatbot','mutter','robot','k']
         self.words_start = ['start','talk','answer','reply','hello','hi']
         self.words_stop = ['stop','exit','quit','quiet','silence']
         self.words_start += self.words_name
