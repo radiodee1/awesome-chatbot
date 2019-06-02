abc88578d917afc478020bedd504e721ec25e328
David Liebman <david.c.liebman@gmail.com>
2019-06-02 12:46:23 -0400

    start using files that will allow gpt2 training.

---
+++ model/tf_gpt2_download_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import os
+import sys
+sys.path.append('..')
+import requests
+from tqdm import tqdm
+from model.settings import hparams
+
+if len(sys.argv) != 2:
+    print('You must enter the model name as a parameter, e.g.: download_model.py 117M')
+    sys.exit(1)
+
+
+model = sys.argv[1]
+
+subdir = os.path.join('models', model)
+subdir2 = os.path.join(hparams['save_dir'], 'tf_gpt2_saved')
+
+if not os.path.exists(subdir2):
+    os.makedirs(subdir2)
+subdir2 = subdir2.replace('\\','/') # needed for Windows
+
+for filename in ['checkpoint','encoder.json','hparams.json','model.ckpt.data-00000-of-00001', 'model.ckpt.index', 'model.ckpt.meta', 'vocab.bpe']:
+
+    r = requests.get("https://storage.googleapis.com/gpt-2/" + subdir + "/" + filename, stream=True)
+
+    with open(os.path.join(subdir2, filename), 'wb') as f:
+        file_size = int(r.headers["content-length"])
+        chunk_size = 1000
+        with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
+            # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+                pbar.update(chunk_size)
