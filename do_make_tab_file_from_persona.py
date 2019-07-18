25c3272af5ed52f86e94a4e8380029681d03b030
David Liebman <david.c.liebman@gmail.com>
2019-07-18 11:31:49 -0400

    filter out *dont* strings

--- do_make_tab_file_from_persona.py
+++ do_make_tab_file_from_persona.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 
 import json
+import re
 
 personachat_file = './raw/personachat_self_original.json'
 
 personachat_tab_file = './data/raw.txt'
+
+filter_dont = True
+print_to_screen = False
 
 with open(personachat_file, "r", encoding="utf-8") as f:
     dataset = json.loads(f.read())
 
 
 # Tokenize and encode the dataset using our loaded GPT tokenizer
-def tokenize(obj, handle):
+def tokenize(obj, handle, space=' '):
 
     if isinstance(obj, str):
-        print(obj)
-        if not (obj.startswith('__') or obj.endswith('__')):
+        if print_to_screen: print(space, len(space), obj)
+        l = len(re.sub('don','', obj))
+        if (not (obj.startswith('__') or obj.endswith('__'))) and (not filter_dont or l == len(obj)):
             handle.write(obj + '\n')
         else:
-            print('SILENCE tag')
+            if print_to_screen: print(space, 'SILENCE tag')
         return obj
         #return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(obj))
     if isinstance(obj, dict):
-        return dict((n, tokenize(o, handle)) for n, o in obj.items())
-    return list(tokenize(o, handle) for o in obj)
+        return dict((n, tokenize(o, handle, space + ' ')) for n, o in obj.items())
+    return list(tokenize(o, handle, space + ' ') for o in obj)
 
 with open(personachat_tab_file,'w') as z:
 
--- do_make_zip_tf_server_copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $#
-echo $@
-
-
-zip -r tens_2_tens.zip saved/t2t_trained_model/export/* data/t2t_data/vocab* $@
-
-echo "use this script with any savable files as the parameter."
