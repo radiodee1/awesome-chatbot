d5fa426b40bc8a20ba4c7528c9c595ebeafd8274
David Liebman <david.c.liebman@gmail.com>
2019-08-02 08:47:11 -0400

    persona corpus

--- do_make_tab_file_from_persona.py
+++ do_make_tab_file_from_persona.py
@@ -3,6 +3,7 @@
 import json
 import re
 import argparse
+import os
 
 personachat_file = './raw/personachat_self_original.json'
 personachat_tab_file = './data/raw.txt'
@@ -16,12 +17,17 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--filter_negative', action='store_true', help='filter out words like *dont*')
 parser.add_argument('--print', action='store_true', help='print to screen')
 parser.add_argument('--separate', action='store_true',  help='make separate to/from files')
+parser.add_argument('--zip', help='name for optional zip file.')
 args = parser.parse_args()
 
 filter_dont = args.filter_negative
 print_to_screen = args.print
 collect_history = True
 multiple_files = args.separate
+
+if args.zip is not None:
+    if not args.zip.endswith('.zip'):
+        args.zip += '.zip'
 
 with open(personachat_file, "r", encoding="utf-8") as f:
     dataset = json.loads(f.read())
@@ -36,6 +42,9 @@ def tokenize(obj, handle, space=' ', label='str:', write=False, recent_label='')
         if print_to_screen: print(space, len(space), label, obj)
 
         l = len(re.sub(' don','', obj))
+        if len(obj) != l:
+            print(obj)
+
         if (not (obj.startswith('__') or obj.endswith('__'))) and (not filter_dont or l == len(obj)):
             if (recent_label == 'history' or not collect_history) and obj != end_of_list:
                 handle.write(obj + '\n')
@@ -60,11 +69,15 @@ with open(personachat_tab_file,'w') as z:
 
 print('---')
 
+file_list = []
+
 if multiple_files:
     with open(personachat_tab_file, 'r') as z:
         zz = z.readlines()
         tr_fr = open(data_dir + 'train.big.from', 'w')
         tr_to = open(data_dir + 'train.big.to', 'w')
+        file_list.append('train.big.from')
+        file_list.append('train.big.to')
         for i in range(len(zz)):
             if i < len(zz) - 1:
                 tr_fr.write(zz[i])
@@ -74,6 +87,8 @@ if multiple_files:
 
         val_fr = open(data_dir + 'valid.big.from', 'w')
         val_to = open(data_dir + 'valid.big.to', 'w')
+        file_list.append('valid.big.from')
+        file_list.append('valid.big.to')
         val_num = int(len(zz) // 20)
         for i in range(len(zz) - val_num * 2 , len(zz) - val_num):
             if i < len(zz) - 1:
@@ -84,10 +99,18 @@ if multiple_files:
 
         test_fr = open(data_dir + 'test.big.from','w')
         test_to = open(data_dir + 'test.big.to','w')
+        file_list.append('test.big.from')
+        file_list.append('test.big.to')
         for i in range(len(zz) - val_num, len(zz)):
             if i < len(zz) - 1:
                 test_fr.write(zz[i])
                 test_to.write(zz[i+1])
         test_fr.close()
         test_to.close()
+
+    if args.zip is not None:
+        print(file_list)
+        os.chdir(data_dir)
+        os.system('zip ' + args.zip + ' ' + ' '.join(file_list))
+        pass
     pass
