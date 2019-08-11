f8db389808aaa3533e79af2ac24daa2dfe3dc9fc
David Liebman <david.c.liebman@gmail.com>
2019-08-11 12:20:24 -0400

    tf server 1.12.0

--- do_make_tf_model_server_download.sh
+++ do_make_tf_model_server_download.sh
@@ -2,10 +2,10 @@
 
 cd raw
 
-wget https://github.com/emacski/tensorflow-serving-arm/releases/download/v1.14.0/tensorflow_model_server-1.14.0-linux_armhf.tar.gz
+wget https://github.com/emacski/tensorflow-serving-arm/releases/download/v1.12.0/tensorflow_model_server-1.12.0-linux_armhf.tar.gz
 
 echo unpack tarball and move executable to sensible location
 echo /usr/local/bin
 
-tar -xvf tensorflow_model_server-1.14.0-linux_armhf.tar.gz
+tar -xvf tensorflow_model_server-1.12.0-linux_armhf.tar.gz
 
