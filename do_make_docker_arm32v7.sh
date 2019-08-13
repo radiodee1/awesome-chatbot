446590e9c3f1a4b049d2ed2a1cf0476a80874609
David Liebman <david.c.liebman@gmail.com>
2019-08-13 08:22:02 -0400

    docker script for pi

---
+++ do_make_docker_arm32v7.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker pull emacski/tensorflow-serving:1.14.0-arm32v7
+
+TESTDATA="$(pwd)/saved/t2t_train/chat_10/export/1564940385/"
+
+docker run -t --rm -p 8501:8501 \
+    -v "$TESTDATA" \
+    -e MODEL_NAME=chat \
+    emacski/tensorflow-serving:1.14.0-arm32v7  # &
