e96a2bdeba7e58cce3599a4492f15b097fbf41e5
David Liebman <david.c.liebman@gmail.com>
2019-08-13 11:48:31 -0400

    arm

--- do_make_docker_arm32v7.sh
+++ do_make_docker_arm32v7.sh
@@ -5,6 +5,6 @@ docker pull emacski/tensorflow-serving:1.14.0-arm32v7
 TESTDATA="$(pwd)/saved/t2t_train/" #chat_10/export/1564940385/"
 
 docker run -t --rm -p 8500:8500 \
-    -v "$TESTDATA:chat_10/" \
+    -v "$TESTDATA:/" \
     -e MODEL_NAME=chat_10 -e MODEL_BASE_PATH='/' \
     emacski/tensorflow-serving:1.14.0-arm32v7  # &
