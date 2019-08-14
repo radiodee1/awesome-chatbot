ee971a14552bc0366bcac5abce542f8ee70a4389
David Liebman <david.c.liebman@gmail.com>
2019-08-14 18:29:23 -0400

    shell script

--- do_make_docker_arm32v7.sh
+++ do_make_docker_arm32v7.sh
@@ -13,7 +13,7 @@ EXPORT_NUM="1564940385"
 #docker run --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/${EXPORT_NUM}/,dst=/${MODEL_NAME}  --entrypoint ls emacski/tensorflow-serving:1.14.0-arm32v7  -hal chat_10
 docker run --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/,dst=/${MODEL_NAME}  --entrypoint ls emacski/tensorflow-serving:1.14.0-arm32v7  -hal chat_10/export/
 
-exit
+
 
 #cd $TESTDATA/$MODEL_NAME/.
 
@@ -21,7 +21,7 @@ docker run -t --rm -p 8500:8500 \
     --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/${EXPORT_NUM}/,dst=/${MODEL_NAME} \
     -e MODEL_NAME=$MODEL_NAME -e MODEL_BASE_PATH="" \
     --entrypoint tensorflow_model_server emacski/tensorflow-serving:1.14.0-arm32v7 \
-    --port=8500 --model_name=${MODEL_NAME} --model_base_path=/$MODEL_NAME \
+    --port=8500 --model_name=${MODEL_NAME} --model_base_path=/$MODEL_NAME/export/1 \
     #emacski/tensorflow-serving:1.14.0-arm32v7  # &
 
 #    --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/${EXPORT_NUM}/,dst=/${MODEL_NAME} \
