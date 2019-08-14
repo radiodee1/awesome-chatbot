b113e0e0de53f45da080bceb2316935847edca30
David Liebman <david.c.liebman@gmail.com>
2019-08-14 18:35:44 -0400

    shell script

--- do_make_docker_arm32v7.sh
+++ do_make_docker_arm32v7.sh
@@ -18,10 +18,10 @@ docker run --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/,dst=/${MODEL_NAME}  
 #cd $TESTDATA/$MODEL_NAME/.
 
 docker run -t --rm -p 8500:8500 \
-    --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/,dst=/${MODEL_NAME} \
+    --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/${EXPORT_NUM}/,dst=/${MODEL_NAME} \
     -e MODEL_NAME=$MODEL_NAME -e MODEL_BASE_PATH="" \
     --entrypoint tensorflow_model_server emacski/tensorflow-serving:1.14.0-arm32v7 \
-    --port=8500 --model_name=${MODEL_NAME} --model_base_path=/$MODEL_NAME/export/1 \
+    --port=8500 --model_name=${MODEL_NAME} --model_base_path=/$MODEL_NAME/export/${EXPORT_NUM} \
     #emacski/tensorflow-serving:1.14.0-arm32v7  # &
 
 #    --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/${EXPORT_NUM}/,dst=/${MODEL_NAME} \
