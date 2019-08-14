1628a25212b78664c28f9ccb7936a8f27411c10c
David Liebman <david.c.liebman@gmail.com>
2019-08-14 17:20:57 -0400

    entrypoint

--- do_make_docker_arm32v7.sh
+++ do_make_docker_arm32v7.sh
@@ -6,17 +6,17 @@ TESTDATA="$(pwd)/saved/t2t_train"
 MODEL_NAME="chat_10"
 EXPORT_NUM="1564940385"
 
-cp  -R --parent $TESTDATA/$MODEL_NAME/export/$EXPORT_NUM/* $TESTDATA/$MODEL_NAME/.
+#cp  -R --parent $TESTDATA/$MODEL_NAME/export/$EXPORT_NUM/* $TESTDATA/$MODEL_NAME/.
 
 #ls -hal $TESTDATA/$MODEL_NAME
 
-docker run --mount type=bind,src=${TESTDATA}/,dst=/${MODEL_NAME}  --entrypoint ls emacski/tensorflow-serving:1.14.0-arm32v7  -hal
-exit
+#docker run --mount type=bind,src=${TESTDATA}/,dst=/${MODEL_NAME}  --entrypoint ls emacski/tensorflow-serving:1.14.0-arm32v7  -hal
+#exit
 
-cd $TESTDATA/$MODEL_NAME/.
+#cd $TESTDATA/$MODEL_NAME/.
 
 docker run -t --rm -p 8500:8500 \
-    --mount type=bind,src=${TESTDATA}/,dst=/${MODEL_NAME} \
+    --mount type=bind,src=${TESTDATA}/${MODEL_NAME}/export/${EXPORT_NUM}/,dst=/${MODEL_NAME} \
     -e MODEL_NAME=$MODEL_NAME -e MODEL_BASE_PATH="" \
     --entrypoint tensorflow_model_server emacski/tensorflow-serving:1.14.0-arm32v7 \
     --port=8500 --model_name=${MODEL_NAME} --model_base_path=/$MODEL_NAME \
