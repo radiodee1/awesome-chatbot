ee19199ad3960fa54e069e407691c31d933a96e1
David Liebman <david.c.liebman@gmail.com>
2019-08-14 14:18:21 -0400

    entrypoint

--- do_make_docker_arm32v7.sh
+++ do_make_docker_arm32v7.sh
@@ -10,8 +10,9 @@ cp  -R --parent $TESTDATA/$MODEL_NAME/export/$EXPORT_NUM/* $TESTDATA/$MODEL_NAME
 
 ls -hal $TESTDATA/$MODEL_NAME
 
-#docker run --mount type=bind,src=${TESTDATA}/,dst=/${MODEL_NAME}  --entrypoint ls emacski/tensorflow-serving:1.14.0-arm32v7 -hal
+docker run --mount type=bind,src=${TESTDATA}/,dst=/${MODEL_NAME}  --entrypoint ls emacski/tensorflow-serving:1.14.0-arm32v7 -hal
 
+exit
 cd $TESTDATA/$MODEL_NAME/.
 
 docker run -t --rm -p 8500:8500 \
