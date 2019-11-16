a7bfd82fa528efea04e0e3a20b8cfd4d5331dd18
David Liebman <david.c.liebman@gmail.com>
2019-11-16 17:11:38 -0500

    different sizes

--- model/tf_gpt2_download_model.py
+++ model/tf_gpt2_download_model.py
@@ -7,30 +7,33 @@ import requests
 from tqdm import tqdm
 from model.settings import hparams
 
+#hparams['data_dir'] = '../data/'
+
 if len(sys.argv) != 2:
     print('You must enter the model name as a parameter, e.g.: download_model.py 117M')
     sys.exit(1)
 
-
 model = sys.argv[1]
-
-subdir = os.path.join('models', model)
-subdir2 = os.path.join(hparams['data_dir'], 'tf_gpt2_data')
-
-if not os.path.exists(subdir2):
-    os.makedirs(subdir2)
-subdir2 = subdir2.replace('\\','/') # needed for Windows
 
 if model == '117M':
     savedir = ''
 else:
     savedir = '/' + model + '/'
 
+subdir = os.path.join('models', model)
+subdir2 = os.path.join(hparams['data_dir'], 'tf_gpt2_data') + "/" + savedir
+
+if not os.path.exists(subdir2):
+    print(subdir2)
+    os.makedirs(subdir2)
+subdir2 = subdir2.replace('\\','/') # needed for Windows
+
+
 for filename in ['checkpoint','encoder.json','hparams.json','model.ckpt.data-00000-of-00001', 'model.ckpt.index', 'model.ckpt.meta', 'vocab.bpe']:
 
     r = requests.get("https://storage.googleapis.com/gpt-2/" + subdir + "/" + filename, stream=True)
 
-    with open(os.path.join(subdir2, savedir, filename), 'wb') as f:
+    with open(os.path.join(subdir2, filename), 'wb') as f:
         file_size = int(r.headers["content-length"])
         chunk_size = 1000
         with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
