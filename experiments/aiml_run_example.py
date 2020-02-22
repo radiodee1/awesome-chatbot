28395b3c599f0ccaf53914d0ff1419a9a178559b
David Liebman <david.c.liebman@gmail.com>
2020-02-22 11:53:53 -0500

    delete one xml file

--- experiments/aiml_run_example.py
+++ experiments/aiml_run_example.py
@@ -6,7 +6,7 @@ import aiml
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 kernel.verbose(False)
-kernel.learn("std_startup.xml")
+kernel.learn("../data/std_startup.xml")
 #kernel.respond("load aiml b")
 
 # Press CTRL-C to break this loop
