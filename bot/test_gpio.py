c8710b1bdf9d5f00ca5ecb4645696b81f30599fa
David Liebman <david.c.liebman@gmail.com>
2020-03-13 14:30:57 -0400

    test gpio

---
+++ bot/test_gpio.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import time
+
+try:
+    import RPi.GPIO as GPIO
+    led_pin_a = 12
+    led_pin_b = 16
+    print('load gpio')
+except:
+    pin_skip = True
+    print('no load gpio')
+
+
+class Game:
+    def __init__(self):
+        print('hello - gpio test')
+
+    def pin_setup(self):
+        if pin_skip: return
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(led_pin_a, GPIO.OUT)
+        GPIO.setup(led_pin_b, GPIO.OUT)
+
+    def pin_a_on(self):
+        if pin_skip: return
+        GPIO.output(led_pin_a, GPIO.HIGH)
+        GPIO.output(led_pin_b, GPIO.LOW)
+
+    def pin_a_off(self):
+        if pin_skip: return
+        GPIO.output(led_pin_a, GPIO.LOW)
+        GPIO.output(led_pin_b, GPIO.HIGH)
+
+if __name__ == '__main__':
+
+    g = Game()
+    g.pin_setup()
+    while True:
+        print('light on')
+        g.pin_a_on()
+        time.sleep(1)
+        print('light off')
+        g.pin_a_off()
+        time.sleep(1)
