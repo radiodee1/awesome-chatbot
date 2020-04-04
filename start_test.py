c49c81b6ca34764addcb38a7d6eaa35c42c22cea
David Liebman <david.c.liebman@gmail.com>
2020-04-04 12:14:45 -0400

    try cloud load

--- start_test.py
+++ start_test.py
@@ -2,12 +2,16 @@
 # Requires PyAudio and PySpeech.
 from __future__ import division, print_function
 
+try:
+    import re
+    import sys
 
-import re
-import sys
-
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-import pyaudio
-from six.moves import queue
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    import pyaudio
+    from six.moves import queue
+except:
+    pass
+finally:
+    print('google cloud load')
