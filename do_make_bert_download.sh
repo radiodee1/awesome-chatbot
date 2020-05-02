7dfa3b485ef0e758b1f482831d0b4f7b895b4e10
David Liebman <david.c.liebman@gmail.com>
2020-05-02 07:36:08 -0400

    make bert download

---
+++ do_make_bert_download.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd data
+mkdir uncased_L-6_H-512_A-8
+cd uncased_L-6_H-512_A-8
+
+wget https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-6_H-512_A-8.zip
+
+unzip uncased_L-6_H-512_A-8.zip
