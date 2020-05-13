ad275fb937b2388dbd5ec7af9afdeb4c79ac22d6
David Liebman <david.c.liebman@gmail.com>
2020-05-13 19:39:26 -0400

    docker files

--- docker/docker_test_armv7/do_launch.sh
+++ docker/docker_test_armv7/do_launch.sh
@@ -6,5 +6,7 @@ cd ../..
 
 echo ${PWD}
 
-docker build --tag awesome:1.0 ./docker/docker_test_x86_64/
+docker build --tag awesome:1.0 ./docker/docker_test_armv7/
 #docker run --publish 8000:8080 awesome:1.0
+./do_make_docker_arm32v7.sh
+
--- docker/docker_test_x86_64/do_launch.sh
+++ docker/docker_test_x86_64/do_launch.sh
@@ -2,9 +2,10 @@
 
 DOCKER_BUILDKIT=1
 
-cd ../..
+cd ../../
 
 echo ${PWD}
 
-docker build --tag awesome:1.0 ./docker/docker_test_x86_64/
-#docker run --publish 8000:8080 awesome:1.0
+docker build --tag awesome:1.0 -f ${PWD}/Dockerfile.amd64 .
+
+docker run -p 8001:8001 --entrypoint "./game_sr.py" awesome:1.0
