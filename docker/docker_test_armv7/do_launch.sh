f41d3832419825fa3dd552b01eac1a987c07d0f3
David Liebman <david.c.liebman@gmail.com>
2020-05-13 17:28:13 -0400

    arm

--- docker/docker_test_armv7/do_launch.sh
+++ docker/docker_test_armv7/do_launch.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-docker build --tag awesome:1.0 .
+DOCKER_BUILDKIT=1
 
+cd ../..
+
+echo ${PWD}
+
+docker build --tag awesome:1.0 ./docker/docker_test_x86_64/
+#docker run --publish 8000:8080 awesome:1.0
