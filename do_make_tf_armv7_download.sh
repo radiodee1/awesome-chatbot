4734385803e1dc0435b61c70b635387bb099280a
David Liebman <david.c.liebman@gmail.com>
2020-05-18 08:35:33 -0400

    start arm

--- do_make_tf_armv7_download.sh
+++ do_make_tf_armv7_download.sh
@@ -3,3 +3,4 @@
 wget https://github.com/PINTO0309/Tensorflow-bin/raw/master/tensorflow-1.15.0-cp37-cp37m-linux_armv7l.whl
 
 wget https://github.com/radiodee1/pytorch-arm-builds/raw/master/torch-1.4.0a0%2B7f73f1d-cp37-cp37m-linux_armv7l.whl
+
