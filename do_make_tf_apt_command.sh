261d65e9838785eb99d79892470056b8f974450f
David Liebman <david.c.liebman@gmail.com>
2020-05-26 13:09:47 -0400

    apt command

---
+++ do_make_tf_apt_command.sh
@@ -0,0 +1,11 @@
+sudo apt-get update \
+    && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mpg123 python3-pyaudio vim python3 python3-pip curl mpg321 \
+    alsa-utils  libasound2-plugins wget build-essential python-opencv python3-opencv cython3 cython python3-scipy \
+    python3-matplotlib python3-cffi python3-greenlet python3-pycparser python3-gevent python3-grpcio python3-h5py \
+    libxml2-dev libxslt-dev python3-lxml libopenblas-dev pciutils alsa alsa-base libhdf5-dev libhd-dev apt-transport-https \
+    ca-certificates \
+    && sudo rm -fr /var/lib/apt/lists/*
+
+sudo apt-get -y remove python3-mpi4py
+
+sudo pip3 install tensor2tensor==1.15.5 tensorflow-serving-api==1.15.0
