bfe10af28c5d60b1575a6874a2d366471f5ae2c4
David Liebman <david.c.liebman@gmail.com>
2020-05-26 18:05:09 -0400

    arm tf 1.15

--- do_make_tf_armv7_download.sh
+++ do_make_tf_armv7_download.sh
@@ -7,12 +7,9 @@ wget --no-check-certificate https://www.piwheels.org/simple/opencv-python/opencv
 
 #wget --no-check-certificate https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.14.0-buster/tensorflow-1.14.0-cp37-none-linux_armv7l.whl
 
-#wget --no-check-certificate https://www.piwheels.org/simple/tensorflow/tensorflow-1.13.1-cp37-none-linux_armv7l.whl #sha256=25f4ff027beec1e568baf8e90a07bad59d354560533d6b37318b9efeb70beeb1
-
-wget --no-check-certificate https://github.com/PINTO0309/TensorflowLite-bin/raw/master/1.15.2/tflite_runtime-1.15.2-cp37-cp37m-linux_armv7l.whl
+###wget --no-check-certificate https://www.piwheels.org/simple/tensorflow/tensorflow-1.13.1-cp37-none-linux_armv7l.whl #sha256=25f4ff027beec1e568baf8e90a07bad59d354560533d6b37318b9efeb70beeb1
 
 wget --no-check-certificate https://github.com/PINTO0309/Tensorflow-bin/raw/master/tensorflow-1.15.0-cp37-cp37m-linux_armv7l.whl
 
 wget --no-check-certificate https://www.piwheels.org/simple/h5py/h5py-2.9.0-cp37-cp37m-linux_armv7l.whl #sha256=3734f3af6d58f38c84e1b965c02da9f0466356d832bb32ecfffcbe5aad886453
 
-wget --no-check-certificate https://www.piwheels.org/simple/h5py/h5py-2.6.0-cp37-cp37m-linux_armv7l.whl #sha256=3aa79e85faa04726465588afc2afcad746ea8b86010f18ed70a91f0deb590d55
