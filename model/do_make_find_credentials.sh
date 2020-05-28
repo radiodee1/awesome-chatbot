f169cd36dcce50fd03ea0d6f1c37b99f84b53ccd
David Liebman <david.c.liebman@gmail.com>
2020-05-28 08:34:07 -0400

    find credentials

---
+++ model/do_make_find_credentials.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd ../
+
+echo ${PWD}
+echo ${HOME}
+cp ${HOME}/bin/awesome-sr-*.json ${PWD}/.
+
+ls awesome-sr-*.json > credentials.txt
+
+cat credentials.txt
+
+cp ${HOME}/bin/api_key.txt ${HOME}/bin/cse_id.txt ${PWD}/.
+
+cd model
