7da5beeee212146c251e6505431f7e273a0a920a
David Liebman <david.c.liebman@gmail.com>
2020-06-17 10:29:41 -0400

    apt

--- do_make_tf_aarch64_download.sh
+++ do_make_tf_aarch64_download.sh
@@ -4,7 +4,7 @@ echo "all urls need to be updated!!"
 
 #wget --no-check-certificate https://github.com/radiodee1/pytorch-arm-builds/raw/master/torch-1.4.0a0%2B7f73f1d-cp37-cp37m-linux_armv7l.whl
 
-wget --no-check-certificate https://www.piwheels.org/simple/opencv-python/opencv_python-4.1.1.26-cp37-cp37m-linux_armv7l.whl #sha256=f600ca8c1ba09c8f974b322bfd23662971cb051ab41ff2278042622c201c6a2a
+#wget --no-check-certificate https://www.piwheels.org/simple/opencv-python/opencv_python-4.1.1.26-cp37-cp37m-linux_armv7l.whl #sha256=f600ca8c1ba09c8f974b322bfd23662971cb051ab41ff2278042622c201c6a2a
 
 wget --no-check-certificate https://github.com/PINTO0309/Tensorflow-bin/raw/master/tensorflow-1.15.0-cp37-cp37m-linux_armv7l.whl
 
