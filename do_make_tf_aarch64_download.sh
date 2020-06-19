ddbbd7e854bab9a88541628ea093c5c1792ccfe7
David Liebman <david.c.liebman@gmail.com>
2020-06-19 13:32:16 -0400

    requirements

--- do_make_tf_aarch64_download.sh
+++ do_make_tf_aarch64_download.sh
@@ -4,9 +4,10 @@ echo "all urls need to be updated!!"
 
 #wget --no-check-certificate https://github.com/radiodee1/pytorch-arm-builds/raw/master/torch-1.4.0a0%2B7f73f1d-cp37-cp37m-linux_armv7l.whl
 
-#wget --no-check-certificate https://www.piwheels.org/simple/opencv-python/opencv_python-4.1.1.26-cp37-cp37m-linux_armv7l.whl #sha256=f600ca8c1ba09c8f974b322bfd23662971cb051ab41ff2278042622c201c6a2a
+#
 
-wget --no-check-certificate https://github.com/PINTO0309/Tensorflow-bin/raw/master/tensorflow-1.15.0-cp37-cp37m-linux_armv7l.whl
+wget --no-check-certificate https://github.com/PINTO0309/Tensorflow-bin/raw/master/tensorflow-1.15.0-cp37-cp37m-linux_aarch64.whl
 
-#wget --no-check-certificate https://www.piwheels.org/simple/h5py/h5py-2.9.0-cp37-cp37m-linux_armv7l.whl #sha256=3734f3af6d58f38c84e1b965c02da9f0466356d832bb32ecfffcbe5aad886453
 
+
+wget --no-check-certificate https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.14.0-buster/tensorflow-1.14.0-cp37-none-linux_aarch64.whl
