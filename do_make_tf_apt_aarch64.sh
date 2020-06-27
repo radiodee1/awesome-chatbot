3343811eabb6977d74d5906ddf6af44088d8fc09
David Liebman <radiodee1@gmail.com>
2020-06-27 14:47:56 -0400

    pygame apt-get build prep

--- do_make_tf_apt_aarch64.sh
+++ do_make_tf_apt_aarch64.sh
@@ -3,9 +3,11 @@ sudo apt-get update \
     alsa-utils  libasound2-plugins wget build-essential python-opencv python3-opencv cython3 cython python3-scipy \
     python3-matplotlib python3-cffi python3-greenlet python3-pycparser python3-gevent  python3-h5py \
     libxml2-dev libxslt-dev python3-lxml libopenblas-dev pciutils alsa-base libhdf5-dev libhd-dev apt-transport-https \
-    ca-certificates portaudio19-dev libssl-dev python3-pygame 
+    ca-certificates portaudio19-dev libssl-dev python3-pygame
 
 
 sudo apt-get -y remove python3-mpi4py
 
 #sudo pip3 install tensor2tensor==1.15.5 tensorflow-serving-api==1.15.0
+
+# sudo apt-get install python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev   libsdl1.2-dev libsmpeg-dev python3-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
--- do_make_tf_apt_amd64.sh
+++ do_make_tf_apt_amd64.sh
@@ -8,3 +8,5 @@ echo 'deb [arch=amd64] https://storage.googleapis.com/tensorflow-serving-apt sta
 sudo curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -
 sudo apt-get update \
     && sudo apt-get install -y tensorflow-model-server
+
+# sudo apt-get install python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev   libsdl1.2-dev libsmpeg-dev python3-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
