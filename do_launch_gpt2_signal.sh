7fb2178ce37e0b2e8b2b4026da54cb46c2ec9ea8
David Liebman <radiodee1@gmail.com>
2020-07-01 07:53:29 -0400

    signal

--- do_launch_gpt2_signal.sh
+++ do_launch_gpt2_signal.sh
@@ -6,6 +6,7 @@ echo $@
 
 TEST_4="--mode=long --basename=test_s2s_movie_openvocab_d300_v15000_len15 --load-babi --lr=0.001 --dropout=0.5 --load-recurrent --units=300 --record-loss --multiplier=0.5 --length=15 --no-vocab "
 TEST_5="--length 12 --top_k 1 $@"
+TEST_6=$@
 
 LAUNCH=launch
 
@@ -21,6 +22,6 @@ export CHATBOT_START="hello. G P T 2 model."
 
 
 cd bot
-python3 game.py ${TEST_5} # 2> /dev/null
+python3 game.py ${TEST_6} # 2> /dev/null
 
 ## this file must be hard coded for any model you want to run
