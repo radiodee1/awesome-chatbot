006049b8ce48de8a118ae87f380e5e94d80d3c8d
David Liebman <radiodee1@gmail.com>
2020-07-06 09:49:19 -0400

    try new copy of run_pretraining.py

--- classifier/do_make_two_bert_interactive.sh
+++ classifier/do_make_two_bert_interactive.sh
@@ -19,10 +19,10 @@ if [ ! -f "../data/${TAB_FILE_OUT}" ]; then
 fi
 
 
-export BERT_BASE_DIR=../data/uncased_L-12_H-768_A-12
+export BERT_BASE_DIR=../data/uncased_L-6_H-512_A-8 #uncased_L-12_H-768_A-12
 export CHAT_DIR=../data/
 
-python3 run_pretraining_two.py \
+python3 run_pretraining.py \
   --input_file=../saved/bert_output/input_file.txt \
   --output_dir=../saved/bert_output \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
