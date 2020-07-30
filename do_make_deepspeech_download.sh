e4e2c39ef601facc9835939408a5141434cc8824
David Liebman <radiodee1@gmail.com>
2020-07-30 13:51:58 -0400

    deepspeech

---
+++ do_make_deepspeech_download.sh
@@ -0,0 +1,5 @@
+
+cd raw/
+
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.scorer
