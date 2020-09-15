d2f363513bc86ece22c370be86614f601354c1c6
David Liebman <radiodee1@gmail.com>
2020-09-15 17:42:23 -0400

    srt break down script

--- do_make_tab_file_from_srt.py
+++ do_make_tab_file_from_srt.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make tab file from srt files.')
     parser.add_argument('--unzip' , help='subtitle files. (place zip files in "raw/srt_zip/" folder)' , action='store_true')
     parser.add_argument('--use-once', help='do not use each line as question and answer.', action='store_true')
+    parser.add_argument('--flat', help='make flat output (no tabs and one sentence per line)', action='store_true')
     args = parser.parse_args()
     args = vars(args)
 
@@ -32,6 +33,8 @@ if __name__ == '__main__':
     filename = 'raw/*.srt'
     flag_use_both = not args['use_once']
 
+    flag_make_flat = args['flat']
+
     filelist = []
     for i in glob.glob(filename):
         filelist.append(i)
@@ -39,6 +42,7 @@ if __name__ == '__main__':
     start_a = ''
     start_b = ''
     start_c = ''
+    start_flat = ''
     print_out = False
     l_out = ''
 
@@ -60,6 +64,10 @@ if __name__ == '__main__':
                         l_out += ' ' + l.strip()
 
                         last_print_bad = num + 1
+                        
+                        if flag_make_flat:
+                            if not start_flat.endswith(start_c):
+                                start_flat += ' ' + start_c.strip()
 
                     if not is_good(l):
 
@@ -74,11 +82,16 @@ if __name__ == '__main__':
                             if print_num % 2 == 0 or print_num < 6:
                                 print_out = True
 
+
                         if print_out:
 
                             if (len(start_c) > 0 and len(start_b) > 0) and (len(l_out) > 0 or not flag_use_both):
-                                z.write(start_c + '\t' + start_b + '\t' + str(1) + '\n')
-                                tot += 1
+                                if not flag_make_flat:
+                                    z.write(start_c + '\t' + start_b + '\t' + str(1) + '\n')
+                                    tot += 1
+                                elif flag_make_flat and len(start_flat.strip()) > 0 :
+                                    z.write(start_flat.strip() + '\n')
+                                    tot += 1
 
                             if not flag_use_both:
                                 start_c = start_b
@@ -95,6 +108,10 @@ if __name__ == '__main__':
                                 l_out = ''
 
                             print_out = False
+
+                        if flag_make_flat:
+                            start_flat = ''
+
                 num += 1
             r.close()
     z.close()
