2cfc677cbfce2378d85591ca93950ece0c87f376
David Liebman <radiodee1@gmail.com>
2020-10-26 14:23:24 -0400

    attn without bmm, general

--- seq_2_seq/seq_2_seq.py
+++ seq_2_seq/seq_2_seq.py
@@ -325,7 +325,7 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.n_layers = n_layers # if not cancel_attention else 1
         self.embed = None # nn.Embedding(target_vocab_size, embed_dim)
-        self.attention_mod = Attn(hidden_dim , method='dot') ## general
+        self.attention_mod = Attn(hidden_dim , method='general') ## general
         self.hidden_dim = hidden_dim
         self.word_mode = cancel_attention #False
         #self.word_mode_b = cancel_attention #False
@@ -623,7 +623,7 @@ class WrapMemRNN(nn.Module):
 
             #context = self.model_6_dec.out_bmm(attn_weights.transpose(2,1), input_unchanged.transpose(2,1)) #, ans_small)
             context = attn_weights.transpose(2,1)
-            #print(context.size(), 'context')
+            print(context.size(), 'context')
             #context = self.model_6_dec.out_bmm(context, input_unchanged) #[:,:,:self.hidden_size])
 
             #context = self.model_6_dec.relu_b(context)
